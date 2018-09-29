981d533fc908e3d61f84b88412092622c2038669
julio.palma.vazquez <julio.palma.vazquez@192.168.1.39>
2018-09-29 20:41:45 +0200

    Add @Override annotation to service implementation methods

--- src/main/java/com/codeone/sample/service/impl/SampleServiceImpl.java
+++ src/main/java/com/codeone/sample/service/impl/SampleServiceImpl.java
@@ -4,11 +4,13 @@ import com.codeone.sample.service.SampleService;
 
 public class SampleServiceImpl implements SampleService {
 
+	@Override
 	public void method1() {
 		// TODO Auto-generated method stub
 
 	}
 
+	@Override
 	public void method2() {
 		// TODO Auto-generated method stub
 
